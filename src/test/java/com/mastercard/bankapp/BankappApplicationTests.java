fac440c79dbf9b3898804093f16ec4a9c17c5c24
Udhaya Shankar Subramanian <udhaysubramanian@udhayas-air.home>
2022-12-01 13:10:48 +0000

    first commit

---
+++ src/test/java/com/mastercard/bankapp/BankappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mastercard.bankapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
